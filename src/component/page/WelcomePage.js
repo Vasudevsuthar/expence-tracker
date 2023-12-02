1bc7048418f6b100d2cc783a43e74a4fed5a2248
Vasudev <Vasudev@codewithvasudev.com>
2023-12-02 22:07:50 +0530

    Task 2

--- src/component/page/WelcomePage.js
+++ src/component/page/WelcomePage.js
@@ -4,7 +4,7 @@ import '../Main.css';
 function WelcomePage() {
   return (
     <div className='lContainer'>
-        <h1>Welcome to website</h1>
+        <h1>Welcome to Expense Tracker</h1>
     </div>
   )
 }
