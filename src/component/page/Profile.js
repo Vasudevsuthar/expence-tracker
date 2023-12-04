176c54ab4c323aa3f97762f85d7700204849ec3e
Vasudev <Vasudev@codewithvasudev.com>
2023-12-04 21:40:49 +0530

    Task 4

--- src/component/page/Profile.js
+++ src/component/page/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import  "./Profile.css";
 import { BsGithub, BsGlobe } from "react-icons/bs";
 import { Link, useNavigate } from "react-router-dom";
@@ -53,6 +53,47 @@ const Profile = () => {
       setName("");
       setProfilePhoto("");
     };
+
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const token = localStorage.getItem("token");
+          const res = await fetch(
+            "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBAYFEF5InzJ-FOrwWVY6_IJ3zMV_ne8Oc",
+            {
+              method: "POST",
+              body: JSON.stringify({
+                idToken: token,
+              }),
+              headers: {
+                "content-type": "application/json",
+              },
+            }
+          );
+          if (res.ok) {
+            const data = await res.json();
+            console.log(data);
+            setName(data.users[0].displayName);
+            setProfilePhoto(data.users[0].photoUrl);
+            console.log("Data fetch Success");
+            //navigate('/home');
+            return data;
+          } else {
+            const data = await res.json();
+            if (data && data.error.message) {
+              setError("Profile not updated successfully- " + data.error.message);
+            } else {
+              setError("Some error occured!! Please try again..");
+            }
+          }
+        } catch (error) {
+          console.error("Error in Profile Updation :", error);
+        }
+      };
+      fetchData();
+    }, []);
+  
 
   return (
     <div className="lcontainer">
