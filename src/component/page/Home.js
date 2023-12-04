105544d84d7483d8b5d8ec368f99e89b2e3354aa
Vasudev <Vasudev@codewithvasudev.com>
2023-12-04 22:40:12 +0530

    Task 5

--- src/component/page/Home.js
+++ src/component/page/Home.js
@@ -1,19 +1,70 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import AuthContext from "../store/auth-context";
 import { Link } from "react-router-dom";
-import classes from './Home.module.css';
+import classes from "./Home.module.css";
 
 const Home = () => {
+  const [error, setError] = useState("");
   const authCtx = useContext(AuthContext);
+
+  const verifyHandler = (e) => {
+    e.preventDefault();
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyBAYFEF5InzJ-FOrwWVY6_IJ3zMV_ne8Oc",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          requestType: "VERIFY_EMAIL",
+          idToken: localStorage.getItem("token"),
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          res.json().then((data) => {
+            console.log(data);
+          });
+        } else {
+          res.json().then((data) => {
+            if (data && data.error && data.error.message) {
+              setError(
+                "Verification mail not sent... try again" + data.error.message
+              );
+            } else {
+              setError("Some error occured!! Please try again..");
+            }
+          });
+        }
+      })
+      .catch((err) => {
+        console.log("Some error in sending verification mail - " + err);
+      });
+  };
 
   return (
     <div className={classes.lContainer}>
-      <h3>Welcome to Expense Tracker</h3>
-      {authCtx.isLoggedIn && (
-        <i>
-          Your Profile is Incomplete <Link to="/profile">Complete Now</Link>
-        </i>
-      )}
+      <div className={classes.header}>
+        <h3 className={classes.h3}>Welcome to Expense Tracker</h3>
+        {authCtx.isLoggedIn && (
+          <i className={classes.i}>
+            Your Profile is Incomplete <Link to="/profile">Complete Now</Link>
+          </i>
+        )}
+      </div>
+      <div className={classes.container}>
+        {authCtx.isLoggedIn && (
+          <div>
+            <h2>Verify your email</h2>
+            <button className={classes.verifyBtn} onClick={verifyHandler}>
+              Verify Email{" "}
+            </button>
+          </div>
+        )}
+        <p className={classes.errorMessage}>{error}</p>
+      </div>
     </div>
   );
 };
