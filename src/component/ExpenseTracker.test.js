86bf12b1c28c19bb76e602921539939e6ff4cc1c
Vasudev <Vasudev@codewithvasudev.com>
2023-12-12 17:09:00 +0530

    Task 24

--- src/component/ExpenseTracker.test.js
+++ src/component/ExpenseTracker.test.js
@@ -5,6 +5,8 @@ import { MemoryRouter } from "react-router-dom";
 import Signup from "./page/Singup";
 import Profile from "./page/Profile";
 import ForgotPassword from "./page/ForgotPass";
+import userEvent from "@testing-library/user-event";
+import { fireEvent } from "@testing-library/react";
 
 test("Test First React app case", () => {
   render(<Home />);
@@ -23,7 +25,6 @@ test("Test Second React app case", () => {
   const text = screen.getByText("Expense Tracker");
   expect(text).toBeInTheDocument();
 });
-
 
 test("Test Third React app case", () => {
   render(
@@ -111,4 +112,114 @@ test("Test Tenth React app case", () => {
 
   const text = screen.getByText("Forget Password");
   expect(text).toBeInTheDocument();
+});
+
+test("If the login button was Not clicked", () => {
+  render(
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>
+  );
+
+  const loginButton = screen.getByRole("button");
+  userEvent.click(loginButton);
+});
+test("If the login button was Not clicked", () => {
+  render(
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>
+  );
+
+  const loginButton = screen.getByRole("button");
+  fireEvent.click(loginButton);
+});
+
+test("If the singup button was Not clicked", () => {
+  render(
+    <MemoryRouter>
+      <Signup />
+    </MemoryRouter>
+  );
+
+  const SingupButton = screen.getByRole("button");
+  userEvent.click(SingupButton);
+});
+test("If the singup button was Not clicked", () => {
+  render(
+    <MemoryRouter>
+      <Signup />
+    </MemoryRouter>
+  );
+
+  const SingupButton = screen.getByRole("button");
+  fireEvent.click(SingupButton);
+});
+
+test("If the forgotpass button was Not clicked", () => {
+  render(
+    <MemoryRouter>
+      <ForgotPassword />
+    </MemoryRouter>
+  );
+
+  const forgotpassButton = screen.getByRole("button");
+  userEvent.click(forgotpassButton);
+});
+
+test("If the forgotpass button was not clicked", () => {
+  render(
+    <MemoryRouter>
+      <ForgotPassword />
+    </MemoryRouter>
+  );
+
+  const forgotpassButton = screen.getByRole("button");
+  fireEvent.click(forgotpassButton);
+});
+
+test('Clicking on"Login here" link ', () => {
+  render(
+    <MemoryRouter>
+      <ForgotPassword />
+    </MemoryRouter>
+  );
+
+  const loginLink = screen.getByRole("link");
+
+  userEvent.click(loginLink);
+});
+
+test('Clicking on "Sing Up" link ', () => {
+  render(
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>
+  );
+
+  const singupLink = screen.getByRole("link", { name: /sign up/i });
+
+  userEvent.click(singupLink);
+});
+test('Clicking on "Sing Up" link ', () => {
+  render(
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>
+  );
+
+  const singupLink = screen.getByRole("link", { name: /Forgot Password?/i });
+
+  userEvent.click(singupLink);
+});
+test('Clicking on "Sing Up" link ', () => {
+  render(
+    <MemoryRouter>
+      <Signup />
+    </MemoryRouter>
+  );
+
+  const loginLink = screen.getByRole("link");
+
+  userEvent.click(loginLink);
 });
